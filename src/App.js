4125fc085360f97f9ab1b9c2f7a853d4fd7e27e6
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-10-14 17:17:14 +0200

    primo es

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { hello } from './components/es-1';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      {hello}
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/es-1.js
@@ -0,0 +1,3 @@
+export const hello = <h1>السلام عليكم Hello, World!
+    
+</h1>
